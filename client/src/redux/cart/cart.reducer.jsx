cb3c066efe0e83bc96285914b78695fd9365fb33
ArunKumar <ArunKumar.D@cognizant.com>
2021-05-05 14:50:21 -0400

    Added code for Cart

--- client/src/redux/cart/cart.reducer.jsx
+++ client/src/redux/cart/cart.reducer.jsx
@@ -1,6 +1,5 @@
-
-import { addItemToCart, removeItem } from './cart.utils';
-import CartActionTypes from './cart.types';
+import { addItemToCart, removeItem } from "./cart.utils";
+import CartActionTypes from "./cart.types";
 
 const INITIAL_STATE = {
   hidden: true,
@@ -9,33 +8,40 @@ const INITIAL_STATE = {
 
 const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-  case CartActionTypes.TOGGLE_CART_HIDDEN:
-    return {
-      ...state,
-      hidden: !state.hidden,
-    };
-  case CartActionTypes.ADD_ITEM:
-    return {
-      ...state,
-      cartItems: addItemToCart(state.cartItems, action.payload),
-    };
-  case CartActionTypes.REMOVE_ITEM:
-    return {
-      ...state,
-      cartItems: removeItem(state.cartItems, action.payload),
-    };
-  case CartActionTypes.CLEAR_ITEM_FROM_CART:
-    return {
-      ...state,
-      cartItems: state.cartItems.filter((cartItem) => cartItem.id !== action.payload.id),
-    };
-  case CartActionTypes.CLEAR_CART:
-    return {
-      ...state,
-      cartItems: [],
-    };
-  default:
-    return state;
+    case CartActionTypes.TOGGLE_CART_HIDDEN:
+      return {
+        ...state,
+        hidden: !state.hidden,
+      };
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload),
+      };
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItem(state.cartItems, action.payload),
+      };
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.id !== action.payload.id
+        ),
+      };
+    case CartActionTypes.CLEAR_CART:
+      return {
+        ...state,
+        cartItems: [],
+      };
+    case CartActionTypes.SET_CART_FROM_FIREBASE:
+      return {
+        ...state,
+        cartItems: action.payload,
+      };
+    default:
+      return state;
   }
 };
 
