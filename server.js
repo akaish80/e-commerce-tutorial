2d35e143fc29a0c655551d30644d479995a55ba1
ArunKumar <ArunKumar.D@cognizant.com>
2021-05-05 10:43:50 -0400

    Updated Server file

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const path = require('path');
-const compression = require('compression');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+const compression = require("compression");
 
-const enforce = require('express-sslify');
+const enforce = require("express-sslify");
 
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.port || 5000;
@@ -17,37 +17,39 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-if (process.env.NODE_ENV === 'production') {
- app.use(compression);
- app.use(enforce.HTTPS({trustProtoHeader: true}));
- app.use(express.static(path.join(__dirname, 'client/build')));
+console.log(process.env.NODE_ENV);
 
- app.get('/service-worker.js', (req, res) => {
-   res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
- });
+if (process.env.NODE_ENV === "production") {
+  app.use(compression);
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
- app.get('*', function(req, res) {
-   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
- });
+  app.get("/service-worker.js", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
+  });
 }
 
-app.listen(port, error => {
- if (error) throw error;
- console.log('server running on port ' + port);
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
-app.post('/payment', (req, res) => {
- const body = {
-  source: req.body.token.id,
-  amount: req.body.amount,
-  currency: 'usd',
- };
+app.listen(port, (error) => {
+  if (error) throw error;
+  console.log("server running on port " + port);
+});
 
- stripe.charges.create(body, (stripeErr, stripeRes) => {
-  if (stripeErr) {
-   res.status(500).send({ error: stripeErr });
-  } else {
-   res.status(200).send({ success: stripeRes });
-  }
- });
+app.post("/payment", (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: "usd",
+  };
+
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
 });
